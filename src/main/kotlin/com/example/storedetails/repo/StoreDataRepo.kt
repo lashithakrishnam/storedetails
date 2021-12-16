ef7104184d86fd92d9dc192e3ff6c6a63f0597c1
lashithakrishnam <krishnamlashi2111@gmail.com>
2021-12-16 14:14:39 +0530

    query updated

--- src/main/kotlin/com/example/storedetails/repo/StoreDataRepo.kt
+++ src/main/kotlin/com/example/storedetails/repo/StoreDataRepo.kt
@@ -14,9 +14,10 @@ import java.time.LocalDate
 @Repository
 interface StoreDataRepo : JpaRepository<StoreData,Long> {
 
-    @Query("SELECT store.name as name ,store.status as status ,store.lastUpdated as updateAt, ap as addressPeriod FROM StoreData store Join store.addressPeriod ap where (  ( (ap.dateValidFrom <= :refDate) and ((ap.dateValidUntill = null) or (ap.dateValidUntill >= :refDate)) )or ((ap.dateValidUntill >= :refDate)and(:flag=true))  ) ")
+    @Query("SELECT store.name as name ,store.status as status ,store.lastUpdated as updateAt, ap as addressPeriod FROM StoreData store Join store.addressPeriod ap WHERE :refDate BETWEEN ap.dateValidFrom AND COALESCE(ap.dateValidUntill,:refDate)  OR ((ap.dateValidFrom > :refDate)AND(:flag=true))   ")
     fun getStores(@Param("refDate")refDate:LocalDate,@Param("flag")flag:Boolean): List<StoreProjection>
 
     @Query("SELECT store.name FROM StoreData store where (store.name= ?1)and(store.id!=?2)")
     fun storeExistsByName(@Param("name") name:String,@Param("id")id:Long):List<String>
 }
+//( (ap.dateValidFrom <= :refDate) and ((ap.dateValidUntill = null) or (ap.dateValidUntill >= :refDate)) )
